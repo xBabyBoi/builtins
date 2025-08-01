93baf47938ebd321676b84a5d49e03c59b84d848
xbabyboi <elqorichiyoussef@gmail.com>
2025-08-01 08:51:56 +0100

    ss

---
+++ pwd.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pwd.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-qori <yel-qori@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/31 20:38:10 by yel-qori          #+#    #+#             */
+/*   Updated: 2025/07/31 20:38:32 by yel-qori         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int ft_pwd(char **argv)
+{
+    char cwd[4096];
+    
+    if (!getcwd(cwd, sizeof(cwd)))
+    {
+        perror("getcwd failed ");
+        return (1);
+    }
+    ft_putstr_fd(cwd, STDOUT_FILENO);
+    ft_putstr_fd("\n", STDOUT_FILENO);
+    return (0);
+}
